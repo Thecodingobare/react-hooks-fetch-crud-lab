d41f2a880a23729b7dc639609b5aa5c65b0d08b7
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-08-01 10:17:20 +0300

    Completed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((questions) => setQuestions(questions));
+  }, []);
+
+  function onDeleteQuestion(deletedQuestion){
+    let newQuestions = questions.filter(question => question.id !== deletedQuestion.id);
+    setQuestions(newQuestions);
+  }
+
+  function updatedAnswer(updatedQuestion){
+    let newQuestions = questions.map(question =>{
+      if(question.id === updatedQuestion.id) return updatedQuestion;
+      return question;
+    })
+    setQuestions(newQuestions);
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem key={question.id} question={question} onDeleteQuestion={onDeleteQuestion} updatedAnswer={updatedAnswer}/>
+        ))}
+      </ul>
     </section>
   );
 }
